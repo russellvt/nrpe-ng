b8e03bdec031ccc6084ea885bc111c4ac0b9ffdb
Russell M. Van Tassell <russell@geekoncall.net>
2022-06-04 00:18:07 -0700

    Placeholder for future pytest functionality

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+'''
+Placeholder for Python Tests
+
+Ref:
+ - https://docs.pytest.org/en/7.1.x/explanation/goodpractices.html
+ - https://docs.pytest.org/en/7.1.x/example/pythoncollection.html
+'''
+
+def test_placeholder():
+    assert 0 == 0
